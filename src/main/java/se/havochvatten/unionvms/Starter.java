99a387aba9c09475c68438a0203b300ea8730cb6
suzyrgo <suztra0803@skola.goteborg.se>
2021-10-19 12:29:45 +0200

    added web.xml and unionvms/Starter.java

---
+++ src/main/java/se/havochvatten/unionvms/Starter.java
@@ -0,0 +1,17 @@
+package se.havochvatten.unionvms;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class Starter extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
