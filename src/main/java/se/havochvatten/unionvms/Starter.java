3d50c5edba842ed7bdc40b37559c34f59d02cc6e
suzyrgo <suztra0803@skola.goteborg.se>
2021-10-20 15:34:09 +0200

    modified Starter.java

--- src/main/java/se/havochvatten/unionvms/Starter.java
+++ src/main/java/se/havochvatten/unionvms/Starter.java
@@ -1,10 +1,21 @@
 package se.havochvatten.unionvms;
 
+import se.havochvatten.unionvms.ais.simulator.openapi.Server;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 
 public class Starter extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        Server server = new Server();
+        System.out.println("Starting server..");
+        new Thread(server).start();
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
